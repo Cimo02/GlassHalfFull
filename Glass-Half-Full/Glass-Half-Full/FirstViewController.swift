bd7d91851c4c988fb7917e8012ac252aa149c1a4
XCodeClub <xcodeclub@XCodeClubs-Mac-4.local>
2020-05-03 14:15:14 -0400

    Initial commit, ui is mostly setup

---
+++ Glass-Half-Full/Glass-Half-Full/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Glass-Half-Full
+//
+//  Created by XCodeClub on 2020-05-03.
+//  Copyright © 2020 Tyler Ciarmataro. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
