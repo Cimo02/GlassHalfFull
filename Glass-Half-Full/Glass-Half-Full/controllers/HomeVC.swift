810b842b351a4b87d0d316ee8434aaf3fbe501a3
XCodeClub <xcodeclub@XCodeClubs-Mac-4.local>
2020-05-04 15:02:13 -0400

    Added mock data for one week

--- Glass-Half-Full/Glass-Half-Full/controllers/HomeVC.swift
+++ Glass-Half-Full/Glass-Half-Full/controllers/HomeVC.swift
@@ -27,6 +27,10 @@ class HomeVC: UIViewController {
         //try to load days array
         if defaults.object(forKey: "waterData") as? Data ?? nil == nil {
             // no waterHistory exists
+            
+            // create test days DEBUG ONLY DELETE BEFORE SUBMISSION
+            addMockData()
+            
             // have to create a day and add it to daysList
             daysList.append(Day())
             saveDays()
@@ -67,7 +71,7 @@ class HomeVC: UIViewController {
     // user presses button to add a cup
     @IBAction func addCup(_ sender: Any) {
         daysList[daysList.firstIndex(where: {$0.date == currentDate})!].addCup()
-        print("Added, cups is \(daysList[daysList.firstIndex(where: {$0.date == currentDate})!].cups)")
+        print("Added, cups is now \(daysList[daysList.firstIndex(where: {$0.date == currentDate})!].cups)")
         currentCupsLabel!.text = "\(daysList[daysList.firstIndex(where: {$0.date == currentDate})!].cups)"
         saveDays()
     }
@@ -75,7 +79,7 @@ class HomeVC: UIViewController {
     //user presses a button to subtract a cup
     @IBAction func subtractCup(_ sender: Any) {
         daysList[daysList.firstIndex(where: {$0.date == currentDate})!].subtractCup()
-        print("Subtracted, cups is \(daysList[daysList.firstIndex(where: {$0.date == currentDate})!].cups)")
+        print("Subtracted, cups is now \(daysList[daysList.firstIndex(where: {$0.date == currentDate})!].cups)")
         currentCupsLabel!.text = "\(daysList[daysList.firstIndex(where: {$0.date == currentDate})!].cups)"
         saveDays()
     }
@@ -96,6 +100,40 @@ class HomeVC: UIViewController {
            defaults.set(encoded, forKey: "waterData")
            print("saved data")
         }
+    }
+    
+    func addMockData() {
+        print("Adding mock data...")
+        
+        let testDay1 = Day()
+        testDay1.date = "4/28/20"
+        testDay1.cups = 5
+        daysList.append(testDay1)
+        
+        let testDay2 = Day()
+        testDay2.date = "4/29/20"
+        testDay2.cups = 2
+        daysList.append(testDay2)
+        
+        let testDay3 = Day()
+        testDay3.date = "4/30/20"
+        testDay3.cups = 8
+        daysList.append(testDay3)
+        
+        let testDay4 = Day()
+        testDay4.date = "5/1/20"
+        testDay4.cups = 9
+        daysList.append(testDay4)
+        
+        let testDay5 = Day()
+        testDay5.date = "5/2/20"
+        testDay5.cups = 3
+        daysList.append(testDay5)
+        
+        let testDay6 = Day()
+        testDay6.date = "5/3/20"
+        testDay6.cups = 1
+        daysList.append(testDay6)
     }
     
     /*
